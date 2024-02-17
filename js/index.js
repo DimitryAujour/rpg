9affe69c8e480b37a46834a8bd4501f29ffcc404
DimitryAujour <106028495+DimitryAujour@users.noreply.github.com>
2024-02-17 11:07:56 -0500

    I have made the made menu with the start button that will animate the transition towards the next page.Also added the files for the elements

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('start');
+
+    button.addEventListener('click', function() {
+        var audio = document.getElementById('clickSound');
+        audio.play();
+
+        // Add fade-out class to body
+        document.body.classList.add('fade-out');
+
+        // Redirect after the fade-out animation duration
+        setTimeout(function() {
+            window.location.href = 'Introduction.html';
+        }, 2000); // Assuming the fade-out animation duration is 1s (1000ms)
+    });
+});
